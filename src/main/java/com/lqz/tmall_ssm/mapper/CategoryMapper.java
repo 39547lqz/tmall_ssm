3d280ac6e3c408fe01bbba9d3ec41c0cb26e871b
lqz <lqzlqzz@163.com>
2019-04-29 18:04:48 +0800

    测试

---
+++ src/main/java/com/lqz/tmall_ssm/mapper/CategoryMapper.java
@@ -0,0 +1,11 @@
+package com.lqz.tmall_ssm.mapper;
+
+
+import com.lqz.tmall_ssm.pojo.Category;
+
+import java.util.List;
+
+public interface CategoryMapper {
+
+    List<Category> lsit();
+}
---
+++ src/main/java/com/lqz/tmall_ssm/pojo/Category.java
@@ -0,0 +1,15 @@
+package com.lqz.tmall_ssm.pojo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    private Integer id;
+
+    private String name;
+}
